1027f259ed1c71ce6e82e64f0f54f151f8596807
Yan Lyra <yanlyra3@gmail.com>
2022-01-14 00:48:48 -0300

    📝 Added swagger to handle docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "swagger.json";
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use("/users", usersRoutes);
 
 export { app };
